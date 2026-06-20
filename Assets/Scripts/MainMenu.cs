e04dec62640ce41ea7f9a1f8b81cbb5613656183
MinhPhuPNG <phuminh.png@gmail.com>
2026-06-19 22:17:40 -0400

    Initial Tutorial work

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
